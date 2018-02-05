4e1de0d1a816b4d5251c3c59b0ac7929dd20ab92
Akos Szente <akos@szente.info>
2018-02-05 04:56:38 +0100

    Protecting dashboard from guests

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -23,6 +23,36 @@ class SiteController extends Controller
 		);
 	}
 
+
+	/**
+	 * @return array action filters
+	 */
+	public function filters()
+	{
+		return array(
+			'accessControl', // perform access control for CRUD operations
+		);
+	}
+
+	/**
+	 * Specifies the access control rules.
+	 * This method is used by the 'accessControl' filter.
+	 * @return array access control rules
+	 */
+	public function accessRules()
+	{
+		return array(
+			array('allow',
+				'actions'=>array('dashboard'),
+				'users'=>array('@'),
+			),
+			array('deny',  // deny all users
+				'actions'=>array('dashboard'),
+				'users'=>array('*'),
+			),
+		);
+	}
+
 	/**
 	 * This is the action to handle external exceptions.
 	 */
