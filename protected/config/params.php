75e565efbf682a692a7a05779128cd09571abc71
Akos Szente <akos@szente.info>
2018-02-22 20:31:35 +0000

    Added s02 parameters to params

--- protected/config/params.php
+++ protected/config/params.php
@@ -22,6 +22,10 @@ return array(
 	'pushPem' => '/Users/harfox/uni/l5project/authreq-site/vendors/authreq-sdk/both.pem',
 	'rootca' => '/Users/harfox/uni/l5project/authreq-site/vendors/authreq-sdk/entrust_root_certification_authority.pem',
 
+	//'callbackUrl' => 'https://s02.szente.info/authreq-srv/callback.php',
+	//'pushPem' => '/var/www/cert/both.pem',
+	//'rootca' => '/var/www/cert/entrust_root_certification_authority.pem',
+
 	'twilio_sid' => "AC7856eb312100f145cba79df0268e4db6",
 	'twilio_token' => "ffe8f783bf033ffc746dcca8ed0244cf",
 	'twilio_phonenumber' => '+441412802071',
