8cd5c120492d54d9e860129ae8968fafdc9f08ea
Akos Szente <akos@szente.info>
2018-02-22 20:32:59 +0000

    Removed unnecessary files from yii template

--- protected/config/params.php
+++ protected/config/params.php
@@ -1,22 +1,6 @@
 <?php
 
-// this contains the application parameters that can be maintained via GUI
 return array(
-	// this is displayed in the header section
-	'title'=>'My Yii Blog',
-	// this is used in error pages
-	'adminEmail'=>'webmaster@example.com',
-	// number of posts displayed per page
-	'postsPerPage'=>10,
-	// maximum number of comments that can be displayed in recent comments portlet
-	'recentCommentCount'=>10,
-	// maximum number of tags that can be displayed in tag cloud portlet
-	'tagCloudCount'=>20,
-	// whether post comments need to be approved before published
-	'commentNeedApproval'=>true,
-	// the copyright information displayed in the footer section
-	'copyrightInfo'=>'Copyright &copy; 2009 by My Company.',
-
 	'database' => array('url' => 'localhost', 'user' => 'root', 'password' => 'almakorte', 'srv-db' => 'authreq-srv', 'site-db' => 'authreq-site'),
 	'callbackUrl' => 'http://192.168.0.67:8080/authreq-srv/callback.php',
 	'pushPem' => '/Users/harfox/uni/l5project/authreq-site/vendors/authreq-sdk/both.pem',
